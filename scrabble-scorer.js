588c75a32addbce9d0f231b0039d25944964645d
Justin Portell <justinportell84@gmail.com>
2021-02-27 03:47:14 +0000

    ing

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -90,19 +90,19 @@ let scrabbleScore = function(word) {
 let simpleAlg = {
   name: "Simple Score",
   description: "Each letter is worth 1 point.",
-  scorerFunction: simpleScore
+  scoringFunction: simpleScore
 };
 
 let vowelBonusAlg = {
   name: "Bonus Vowels",
   description: "Vowels are 3 pts, consonants are 1 pt.",
-  scorerFunction: vowelBonusScore
+  scoringFunction: vowelBonusScore
 };
 
 let scrabbleAlg = {
   name: "Scrabble",
   description: "The traditional scoring algorithm.",
-  scorerFunction: scrabbleScore
+  scoringFunction: scrabbleScore
 }
 const scoringAlgorithms = [simpleAlg, vowelBonusAlg, scrabbleAlg];
 
